0ef2272d94d6f6b04244b6c291a74b4699fe54ca
Phil Fitch <mostreluctantly@gmail.com>
2019-09-27 15:31:16 -0700

    films post and get all written and tested

---
+++ e2e/__tests__/films.test.js
@@ -0,0 +1,71 @@
+const request = require('../request');
+const db = require('../db');
+const mongoose = require('mongoose');
+
+describe('films api', () => {
+  beforeEach(() => {
+    return db.dropCollection('films');
+  });
+
+  const data = {
+    title: 'The Matrix',
+    studio: new mongoose.Types.ObjectId,
+    released: 1999,
+    cast: [{
+      role: 'wizard',
+      actor: new mongoose.Types.ObjectId
+    }
+    ]
+  };
+
+  function postFilm(obj) {
+    return request
+      .post('/api/films')
+      .send(obj)
+      .expect(200)
+      .then(({ body }) => body);
+  }
+
+  it('posts a film', () => {
+    return postFilm(data).then(film => {
+      expect(film).toEqual({
+        _id: expect.any(String),
+        __v: 0,
+        title: 'The Matrix',
+        studio: expect.any(String),
+        released: 1999,
+        cast: [{
+          _id: expect.any(String),
+          role: 'wizard',
+          actor: expect.any(String)
+        }]
+      })
+    });
+  });
+
+  it('gets all films', () => {
+    return Promise.all([
+      postFilm(data),
+      postFilm(data),
+      postFilm(data),
+    ])
+      .then(() => {
+        return request.get('/api/films').expect(200);
+      })
+      .then(({ body }) => {
+        expect(body.length).toBe(3);
+        expect(body[0]).toEqual({
+          _id: expect.any(String),
+          __v: 0,
+          title: 'The Matrix',
+          studio: expect.any(String),
+          released: 1999,
+          cast: [{
+            _id: expect.any(String),
+            role: 'wizard',
+            actor: expect.any(String)
+          }]
+        });
+      });
+  });
+})
