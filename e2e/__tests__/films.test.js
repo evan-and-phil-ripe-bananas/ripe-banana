cfff816fffea17adbe7f6cd7a9d8a570a10c712a
evanandrewson <evanandrewson@gmail.com>
2019-09-27 16:42:56 -0700

    working on get film by id route

--- e2e/__tests__/films.test.js
+++ e2e/__tests__/films.test.js
@@ -9,12 +9,13 @@ describe('films api', () => {
 
   const data = {
     title: 'The Matrix',
-    studio: new mongoose.Types.ObjectId,
+    studio: new mongoose.Types.ObjectId(),
     released: 1999,
-    cast: [{
-      role: 'wizard',
-      actor: new mongoose.Types.ObjectId
-    }
+    cast: [
+      {
+        role: 'wizard',
+        actor: new mongoose.Types.ObjectId()
+      }
     ]
   };
 
@@ -34,21 +35,19 @@ describe('films api', () => {
         title: 'The Matrix',
         studio: expect.any(String),
         released: 1999,
-        cast: [{
-          _id: expect.any(String),
-          role: 'wizard',
-          actor: expect.any(String)
-        }]
-      })
+        cast: [
+          {
+            _id: expect.any(String),
+            role: 'wizard',
+            actor: expect.any(String)
+          }
+        ]
+      });
     });
   });
 
   it('gets all films', () => {
-    return Promise.all([
-      postFilm(data),
-      postFilm(data),
-      postFilm(data),
-    ])
+    return Promise.all([postFilm(data), postFilm(data), postFilm(data)])
       .then(() => {
         return request.get('/api/films').expect(200);
       })
@@ -60,12 +59,52 @@ describe('films api', () => {
           title: 'The Matrix',
           studio: expect.any(String),
           released: 1999,
-          cast: [{
-            _id: expect.any(String),
-            role: 'wizard',
-            actor: expect.any(String)
-          }]
+          cast: [
+            {
+              _id: expect.any(String),
+              role: 'wizard',
+              actor: expect.any(String)
+            }
+          ]
         });
       });
   });
-})
+  it('gets a film by id', () => {
+  //   return postFilm(data).then(film => {
+  //     return request
+  //       .post(`/api/actors`)
+  //       .send({
+  //         name: 'Brad Pitt',
+  //         dob: new Date(12 / 18 / 1963),
+  //         pob: 'Shawnee, OK'
+  //       })
+  //       .expect(200)
+  //       .then(() => {
+  //         return request.get(`/api/films/${film._id}`).expect(200);
+  //       })
+  //       .then(({ body }) => {
+  //         console.log(body);
+  //         expect(body).toMatchInlineSnapshot(
+  //           {
+  //             _id: expect.any(String),
+  //             cast: [
+  //               // {
+  //               //   _id: expect.any(String)
+  //               // }
+  //             ]
+  //           },
+  //           `
+  //           Object {
+  //             "__v": 0,
+  //             "_id": Any<String>,
+  //             "cast": Array [],
+  //             "released": 1999,
+  //             "studio": "5d8e9b422b20505223c485ef",
+  //             "title": "The Matrix",
+  //           }
+  //         `
+  //         );
+  //       });
+  //   });
+  // });
+});
